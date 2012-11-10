1f097ae2891ce0da6f9c5b0e616a4079be67cc69
Shriram V <shri314@yahoo.com>
2012-11-10 15:56:08 +0000

    Updates

--- thirdparty/install.sh
+++ thirdparty/install.sh
@@ -1,12 +1,42 @@
-#!/bin/bash -x
+#!/bin/bash
 
+CDPATH=
+SCRIPT_PATH="$(cd "$(dirname "$0")" && pwd)"
+TARGET_PATH="$HOME/.local/incubation-projects"
 
-L="http://downloads.sourceforge.net/project/boost/boost/1.46.1/boost_1_46_1.tar.bz2" &&
-N="$(basename "$L")" &&
-(test -f "$N" || wget "$L") &&
-tar -xjvf "$N" &&
-cd boost_1_46_1 &&
-./bootstrap.sh --prefix=$HOME/.local/incubation-projects/boost_1_46_1 &&
-./bjam --prefix=$HOME/.local/incubation-projects/boost_1_46_1 install &&
-echo "done"
+(
+   cd "$SCRIPT_PATH"
+   set -x; set -e;
 
+   L="http://downloads.sourceforge.net/project/boost/boost/1.46.1/boost_1_46_1.tar.bz2"
+   N="$(basename "$L")"
+   I="$TARGET_PATH/.$N.installed"
+
+   mkdir -p "$TARGET_PATH"
+
+   if [ ! -f "$I" ]
+   then
+      if [ ! -f "$N" ]
+      then
+         wget "$L"
+      fi
+
+      P="boost_1_46_1"
+
+      rm -rf "$P"
+
+      (
+         set -x; set -e;
+
+         tar -xjf "$N"
+         cd "$P"
+         ./bootstrap.sh --prefix="$TARGET_PATH/$P"
+         ./bjam --prefix="$TARGET_PATH/$P" install
+         touch "$I"
+      )
+
+      rm -rf "$P"
+   fi
+
+   echo "Installed $N"
+)
